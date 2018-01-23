204efc6c2ff9b0f00ba36d5d70e137efd908d9c8
nelse003 <nelse003@gmail.com>
2018-01-23 14:08:53 +0000

    Branch for developing selection of grids based on occupancy groups

---
+++ select_occupancy_groups.py
@@ -0,0 +1,10 @@
+import iotbx.pdb
+
+# Process refmac params file to provide occupancy groups
+
+# Use occupancy groups to generate selection of atoms associated to bound & ground structure
+
+# Return an atom selection on the protein hier to be used as selection
+# Provide whether sufficent for co-varying
+
+# Need to consider generation of grid (for summing FoFc) for seperated components.
