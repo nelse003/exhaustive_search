acf8d2ff170ad87410048c192f848f464d416d06
Elliot Nelson <elliot.nelson@dtc.ox.ac.uk>
2018-05-31 08:02:58 +0100

    Repeat soaks: Working on NUDT7, everything else commented out for now.

--- repeat_soaks.py
+++ repeat_soaks.py
@@ -31,7 +31,8 @@ params.settings.processes = 36
 # Run exhaustive search @ 0.01
 #######################################################
 
-NUDT7_copied_dir = "/dls/science/groups/i04-1/elliot-dev/Work/exhaustive_search_data/NUDT7_Copied_atoms"
+NUDT7_copied_dir = ("/dls/science/groups/i04-1/elliot-dev/Work/"
+                   "exhaustive_search_data/NUDT7_Copied_atoms")
 
 # for NUDT7_cov_dataset in filter(lambda x: x.startswith("NUDT7A-x") and
 #                        os.path.isdir(os.path.join(NUDT7_copied_dir, x)), os.listdir(NUDT7_copied_dir)):
@@ -87,26 +88,15 @@ NUDT7_copied_dir = "/dls/science/groups/i04-1/elliot-dev/Work/exhaustive_search_
 
 # NUDT7 NUOOA0000181a
 
-#
+# Copy atoms
+
+# Run exhaustive search
+
+# Plotting occupancy histogram
+
+
 # # NUDT 7 Covalent hits?
 
-# # NUDT22A FMOPL00622a
-# from DSI poised library (x0938-x0976)
-# from DSPL library x0182, x909-x0937
-
-# for in:
-#     params.output.out_dir =
-#     params.input.xtal_name =
-#     params.input.in_path =
-#     params.input.mtz =
-#     params.input.pdb =
-#     params.output.log_dir = os.path.join(params.output.out_dir, "logs")
-#
-#     exhaustive(params)
-
-# NUDT22A 133725a x0421, x1040 - x1059
-# NUDT22A 13663a x0391, x1009 to x1039
-# NUDT22A 13369a x0243, x977 to x1008
 
 # NUDT22_initial_models = "/dls/science/groups/i04-1/elliot-dev/Work/exhaustive_search_data/occupancy_group_with_refinement" #"/dls/labxchem/data/2018/lb18145-55/processing/analysis/initial_model"
 # params.repeat.input.database_path = "/dls/labxchem/data/2018/lb18145-55/processing/database/soakDBDataFile.sqlite"
@@ -132,16 +122,23 @@ NUDT7_copied_dir = "/dls/science/groups/i04-1/elliot-dev/Work/exhaustive_search_
 #     if not os.path.exists(os.path.join(common_out_dir, "NUDT22_from_occ_group_with_refinement")):
 #         os.mkdir(os.path.join(common_out_dir, "NUDT22_from_occ_group_with_refinement"))
 #
-#     if not os.path.exists(os.path.join(common_out_dir, "NUDT22_from_occ_group_with_refinement",params.input.xtal_name)):
-#         os.mkdir(os.path.join(common_out_dir, "NUDT22_from_occ_group_with_refinement",params.input.xtal_name))
+#     if not os.path.exists(os.path.join(common_out_dir,
+#                           "NUDT22_from_occ_group_with_refinement",
+#                           params.input.xtal_name)):
+
+#         os.mkdir(os.path.join(common_out_dir,
+#                               "NUDT22_from_occ_group_with_refinement",
+#                               params.input.xtal_name))
 #
 #     params.output.log_dir = os.path.join(params.output.out_dir, "logs")
 #     if not os.path.exists(params.output.log_dir):
 #         os.mkdir(params.output.log_dir)
 #
-#     params.exhaustive.output.csv_name = params.input.xtal_name + "exhaustive_search_occ_u_iso"
+#     params.exhaustive.output.csv_name = params.input.xtal_name
+#                                         + "exhaustive_search_occ_u_iso"
 #
-#     if os.path.exists(os.path.join(params.output.out_dir,params.exhaustive.output.csv_name)):
+#     if os.path.exists(os.path.join(params.output.out_dir,
+#                       params.exhaustive.output.csv_name)):
 #         continue
 #
 #     params.input.mtz = os.path.join(params.input.in_path,"refine.mtz")
@@ -178,152 +175,128 @@ NUDT7_copied_dir = "/dls/science/groups/i04-1/elliot-dev/Work/exhaustive_search_
 ######################################################
 # NUDT22 Seperate into compound sets
 ######################################################
-prefix = "NUDT22A-x"
-xtals = {}
-# # NUDT22A FMOPL00622a
-# from DSI poised library (x0938-x0976)
-compound_code = 'FMOPL000622a_DSPL'
-xtals['NUDT22A-x0182'] =  compound_code
-start_xtal_num = 909
-end_xtal_num = 937
-for num in range(start_xtal_num, end_xtal_num + 1):
-    xtal_name = prefix + "{0:0>4}".format(num)
-    xtals[xtal_name] = compound_code
-
-# # NUDT22A FMOPL00622a
-# from DSI poised library (x0938-x0976)
-compound_code = 'FMOPL000622a_DSI_poised'
-start_xtal_num = 938
-end_xtal_num = 976
-for num in range(start_xtal_num, end_xtal_num + 1):
-    xtal_name = prefix + "{0:0>4}".format(num)
-    xtals[xtal_name] = compound_code
-
-# NUDT22A 133725a x0421, x1040 - x1059
-
-start_xtal_num = 1040
-end_xtal_num = 1059
-compound_code = '133725a'
-xtals['NUDT22A-x0421'] = compound_code
-for num in range(start_xtal_num, end_xtal_num + 1):
-    xtal_name = prefix + "{0:0>4}".format(num)
-    xtals[xtal_name] = compound_code
-
-# NUDT22A 13369a x0243, x977 to x1008
-
-start_xtal_num = 977
-end_xtal_num = 1008
-compound_code = '13369a'
-xtals['NUDT22A-x0243'] = compound_code
-for num in range(start_xtal_num, end_xtal_num + 1):
-    xtal_name = prefix + "{0:0>4}".format(num)
-    xtals[xtal_name] = compound_code
-
-# NUDT22A 13663a x0391, x1009 to x1039
-
-compound_code = '13363a'
-xtals['NUDT22A-x0391']= compound_code
-start_xtal_num = 1009
-end_xtal_num = 1039
-for num in range(start_xtal_num, end_xtal_num + 1):
-    xtal_name = prefix + "{0:0>4}".format(num)
-    xtals[xtal_name] = compound_code
-
-in_dir = "/dls/science/groups/i04-1/elliot-dev/Work/exhaustive_search_data/occupancy_group_with_refinement"
-
-compound_codes=set()
-for compounds in xtals.values():
-    compound_codes.add(compounds)
-
-for compound in compound_codes:
-    if not os.path.exists(os.path.join(in_dir,compound)):
-        os.mkdir(os.path.join(in_dir,compound))
-
-for xtal, compound_code in xtals.iteritems():
-    if os.path.exists(os.path.join(in_dir,xtal)):
-        shutil.move(os.path.join(in_dir,xtal),os.path.join(in_dir,compound_code))
-
-######################################################
-# NUDT22 Plotting histogram
-######################################################
-
-out_dir = "/dls/science/groups/i04-1/elliot-dev/Work/exhaustive_search_data/repeat_soaks/2018-05-28/NUDT22_from_occ_group_with_refinement/"
-
-for compound in compound_codes:
-
-    es_occs =[]
-
-    # Generate split conformations
-    for dataset in filter(lambda x: x.startswith("NUDT22A-x") and
-                           os.path.isdir(os.path.join(in_dir,compound, x)),
-                                os.listdir(os.path.join(in_dir,compound))):
-
-        if not os.path.exists(os.path.join(in_dir,compound,dataset,"refine.split.ground-state.pdb")):
-            split_params = split_phil.extract()
-            split_params.input.pdb = [os.path.join(in_dir, compound, dataset, "refine.pdb")]
-            split_params.output.log = "cat.log"
-            print(split_params.input.pdb)
-            print(split_phil.format(python_object=split_params).as_str())
-            try:
-                split_conformations(split_params)
-            except IOError:
-                print("Split confs: Issue in parsing:{}".format(dataset))
-                continue
-
-    refine_occs = get_occs(refinement_dir=os.path.join(in_dir,compound),lig_chain="B",
-                           pdb_name="refine.split.bound-state.pdb")
-
-
-    for dataset in filter(lambda x: x.startswith("NUDT22A-x") and
-                                    os.path.isdir(os.path.join(out_dir, compound, x)),
-                          os.listdir(os.path.join(out_dir, compound))):
-        # rename csv files
-        if not os.path.exists(os.path.join(out_dir,compound,dataset,dataset + "_exhaustive_search_occ_u_iso.csv",)):
-            try:
-                shutil.move(os.path.join(out_dir,compound,dataset, dataset +"exhaustive_search_occ_u_iso"),
-                            os.path.join(out_dir,compound,dataset, dataset + "_exhaustive_search_occ_u_iso.csv"))
-            except IOError:
-                print("Issue in parsing:{}".format(dataset))
-                continue
-
-        csv_name = os.path.join(out_dir,compound,dataset, dataset + "_exhaustive_search_occ_u_iso.csv")
-        print(csv_name)
-        try:
-            occ, _, _ = get_minimum_fofc(csv_name)
-        except IOError:
-            print("Issue in parsing:{}".format(dataset))
-            continue
-        es_occs.append(occ)
-
-    params.output.out_dir = os.path.join(out_dir,compound)
-
-    occupancy_histogram_with_exhaustive_search(es_occs, refine_occs, protein_name="NUDT22",
-                                               compound=compound, params=params)
-
-exit()
-# for xtal_name in xtals:
+# prefix = "NUDT22A-x"
+# xtals = {}
+# # # NUDT22A FMOPL00622a
+# # from DSI poised library (x0938-x0976)
+# compound_code = 'FMOPL000622a_DSPL'
+# xtals['NUDT22A-x0182'] =  compound_code
+# start_xtal_num = 909
+# end_xtal_num = 937
+# for num in range(start_xtal_num, end_xtal_num + 1):
+#     xtal_name = prefix + "{0:0>4}".format(num)
+#     xtals[xtal_name] = compound_code
 #
-
-
-
-# refine_occs = refine_occs + get_occs(refinement_dir=NUDT7_copied_dir,lig_chain="E",
-#                        pdb_name="refine.split.bound-state.pdb")
+# # # NUDT22A FMOPL00622a
+# # from DSI poised library (x0938-x0976)
+# compound_code = 'FMOPL000622a_DSI_poised'
+# start_xtal_num = 938
+# end_xtal_num = 976
+# for num in range(start_xtal_num, end_xtal_num + 1):
+#     xtal_name = prefix + "{0:0>4}".format(num)
+#     xtals[xtal_name] = compound_code
 #
-# es_occs =[]
-# for NUDT7_cov_dataset in filter(lambda x: x.startswith("NUDT7A-x") and
-#                        os.path.isdir(
-#                            os.path.join("/dls/science/groups/i04-1/elliot-dev/Work/exhaustive_search_data/repeat_soaks/2018-05-27/NUDT7_copied", x)),
-#                             os.listdir("/dls/science/groups/i04-1/elliot-dev/Work/exhaustive_search_data/repeat_soaks/2018-05-27/NUDT7_copied")):
+# # NUDT22A 133725a x0421, x1040 - x1059
 #
-#     csv_name = os.path.join("/dls/science/groups/i04-1/elliot-dev/Work/exhaustive_search_data/repeat_soaks/2018-05-27/NUDT7_copied",
-#                             NUDT7_cov_dataset, NUDT7_cov_dataset + "_exhaustive_search_occ_u_iso.csv")
-#     print(csv_name)
-#     occ, _, _ = get_minimum_fofc(csv_name)
-#     es_occs.append(occ)
+# start_xtal_num = 1040
+# end_xtal_num = 1059
+# compound_code = '133725a'
+# xtals['NUDT22A-x0421'] = compound_code
+# for num in range(start_xtal_num, end_xtal_num + 1):
+#     xtal_name = prefix + "{0:0>4}".format(num)
+#     xtals[xtal_name] = compound_code
 #
-# print(refine_occs,es_occs)
-# params.output.out_dir = "/dls/science/groups/i04-1/elliot-dev/Work/exhaustive_search_data/repeat_soaks/2018-05-27/NUDT7_copied"
-# occupancy_histogram_with_exhaustive_search(es_occs, refine_occs,protein_name="NUDT7", compound="OX210", params=params)
+# # NUDT22A 13369a x0243, x977 to x1008
+#
+# start_xtal_num = 977
+# end_xtal_num = 1008
+# compound_code = '13369a'
+# xtals['NUDT22A-x0243'] = compound_code
+# for num in range(start_xtal_num, end_xtal_num + 1):
+#     xtal_name = prefix + "{0:0>4}".format(num)
+#     xtals[xtal_name] = compound_code
+#
+# # NUDT22A 13663a x0391, x1009 to x1039
+#
+# compound_code = '13363a'
+# xtals['NUDT22A-x0391']= compound_code
+# start_xtal_num = 1009
+# end_xtal_num = 1039
+# for num in range(start_xtal_num, end_xtal_num + 1):
+#     xtal_name = prefix + "{0:0>4}".format(num)
+#     xtals[xtal_name] = compound_code
+#
+# in_dir = "/dls/science/groups/i04-1/elliot-dev/Work/exhaustive_search_data/occupancy_group_with_refinement"
+#
+# compound_codes=set()
+# for compounds in xtals.values():
+#     compound_codes.add(compounds)
+#
+# for compound in compound_codes:
+#     if not os.path.exists(os.path.join(in_dir,compound)):
+#         os.mkdir(os.path.join(in_dir,compound))
+#
+# for xtal, compound_code in xtals.iteritems():
+#     if os.path.exists(os.path.join(in_dir,xtal)):
+#         shutil.move(os.path.join(in_dir,xtal),os.path.join(in_dir,compound_code))
+#
+# ######################################################
+# # NUDT22 Plotting histogram
+# ######################################################
+#
+# out_dir = "/dls/science/groups/i04-1/elliot-dev/Work/exhaustive_search_data/repeat_soaks/2018-05-28/NUDT22_from_occ_group_with_refinement/"
+#
+# for compound in compound_codes:
+#
+#     es_occs =[]
+#
+#     # Generate split conformations
+#     for dataset in filter(lambda x: x.startswith("NUDT22A-x") and
+#                            os.path.isdir(os.path.join(in_dir,compound, x)),
+#                                 os.listdir(os.path.join(in_dir,compound))):
+#
+#         if not os.path.exists(os.path.join(in_dir,compound,dataset,"refine.split.ground-state.pdb")):
+#             split_params = split_phil.extract()
+#             split_params.input.pdb = [os.path.join(in_dir, compound, dataset, "refine.pdb")]
+#             split_params.output.log = "cat.log"
+#             print(split_params.input.pdb)
+#             print(split_phil.format(python_object=split_params).as_str())
+#             try:
+#                 split_conformations(split_params)
+#             except IOError:
+#                 print("Split confs: Issue in parsing:{}".format(dataset))
+#                 continue
+#
+#     refine_occs = get_occs(refinement_dir=os.path.join(in_dir,compound),lig_chain="B",
+#                            pdb_name="refine.split.bound-state.pdb")
+#
+#
+#     for dataset in filter(lambda x: x.startswith("NUDT22A-x") and
+#                                     os.path.isdir(os.path.join(out_dir, compound, x)),
+#                           os.listdir(os.path.join(out_dir, compound))):
+#         # rename csv files
+#         if not os.path.exists(os.path.join(out_dir,compound,dataset,dataset + "_exhaustive_search_occ_u_iso.csv",)):
+#             try:
+#                 shutil.move(os.path.join(out_dir,compound,dataset, dataset +"exhaustive_search_occ_u_iso"),
+#                             os.path.join(out_dir,compound,dataset, dataset + "_exhaustive_search_occ_u_iso.csv"))
+#             except IOError:
+#                 print("Issue in parsing:{}".format(dataset))
+#                 continue
+#
+#         csv_name = os.path.join(out_dir,compound,dataset, dataset + "_exhaustive_search_occ_u_iso.csv")
+#         print(csv_name)
+#         try:
+#             occ, _, _ = get_minimum_fofc(csv_name)
+#         except IOError:
+#             print("Issue in parsing:{}".format(dataset))
+#             continue
+#         es_occs.append(occ)
+#
+#     params.output.out_dir = os.path.join(out_dir,compound)
+#
+#     occupancy_histogram_with_exhaustive_search(es_occs, refine_occs, protein_name="NUDT22",
+#                                                compound=compound, params=params)
+
 
 
 # DCP2B FMOPL000435a
