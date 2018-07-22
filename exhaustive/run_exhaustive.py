f8f8a930205d5bae0fbc898b1b11b57ecc0cf123
nelse003 <nelse003@gmail.com>
2018-07-22 19:58:48 +0100

    run_exhaustive, swap to collating minima

--- exhaustive/run_exhaustive.py
+++ exhaustive/run_exhaustive.py
@@ -39,90 +39,90 @@ for num in range(start_xtal_num, end_xtal_num + 1):
     xtal_name = prefix + "{0:0>4}".format(num)
     xtals.append(xtal_name)
 
-for xtal_name in xtals:
-
-    params.input.xtal_name = xtal_name
-    params.input.pdb = os.path.join(os.path.join(out_dir, xtal_name, "refine.pdb"))
-    params.input.mtz = os.path.join(os.path.join(out_dir, xtal_name, "refine.mtz"))
-
-    if not os.path.exists(params.input.pdb):
-        continue
-    if not os.path.exists(params.input.mtz):
-        continue
-
-    params.output.out_dir = os.path.join(out_dir, xtal_name)
-    params.output.log_dir = os.path.join(out_dir, xtal_name, "logs")
-    params.exhaustive.output.csv_name = os.path.join(params.output.out_dir, "exhaustive_search.csv")
-
-    if not qsub:
-        exhaustive(params=params)
-        scatter_plot(params.exhaustive.output.csv_name)
-
-    if qsub:
-
-        # pickle params
-
-        with open(os.path.join(out_dir,
-                               xtal_name,
-                               '{}_param.pickle'.format(xtal_name)),'wb') as handle:
-            pickle.dump(params, handle, protocol=pickle.HIGHEST_PROTOCOL)
-
-        # write python script
-
-        python_script = open(os.path.join(out_dir,
-                                          xtal_name,
-                                          "{}_exhaustive.py".format(xtal_name)),'w')
-        python_script.write("import pickle\n"
-                            "import sys\n"
-                            "import os\n"
-                            "sys.path.append(\"/dls/science/groups/i04-1/elliot-dev/"
-                            "Work/exhaustive_search\")\n"
-                            "from exhaustive.exhaustive import run as exhaustive\n"
-                            "import libtbx.phil\n"
-                            "out_dir=\"{}\"\n".format(out_dir) +
-                            "xtal_name=\"{}\"\n".format(xtal_name) +
-                            "with open(os.path.join(out_dir, " \
-                            "xtal_name,\'{}_param.pickle\'.format(xtal_name)),'rb') as handle:\n"
-                            "\tparams = pickle.load(handle)\n"
-                            "exhaustive(params)")
-        python_script.close()
-
-        # write bash script
-        bash_script=open(os.path.join(out_dir, xtal_name,
-                                     "{}_exhaustive.sh".format(xtal_name)),'w')
-        bash_script.write("source /dls/science/groups/i04-1/software/" \
-                          "pandda-update/ccp4/ccp4-7.0/setup-scripts/ccp4.setup-sh\n"
-                          # "export PYTHONPATH=\"${PYTHONPATH}:" \
-                          # "/dls/science/groups/i04-1/elliot-dev/"
-                          # "Work/exhaustive_search/exhaustive\"\n"
-                          "ccp4-python " + os.path.join(out_dir, xtal_name,
-                                          "{}_exhaustive.py".format(xtal_name)))
-        bash_script.close()
-        # submit job
-        os.system("qsub {}".format(os.path.join(out_dir, xtal_name,xtal_name+"_exhaustive.sh")))
-
-# Get exhaustive search minima fofc
-# with open(os.path.join(out_dir,"es_minima.csv"),'wb') as minima_csv:
+# for xtal_name in xtals:
 #
-#     minima_writer = csv.writer(minima_csv, delimiter=',')
+#     params.input.xtal_name = xtal_name
+#     params.input.pdb = os.path.join(os.path.join(out_dir, xtal_name, "refine.pdb"))
+#     params.input.mtz = os.path.join(os.path.join(out_dir, xtal_name, "refine.mtz"))
 #
-#     for xtal_name in xtals:
-#
-#         params.output.out_dir = os.path.join(out_dir, xtal_name)
-#         params.exhaustive.output.csv_name = os.path.join(params.output.out_dir, "exhaustive_search.csv")
-#         if os.path.exists(params.exhaustive.output.csv_name):
-#             os.chdir(os.path.join(out_dir, xtal_name))
-#             scatter_plot(params.exhaustive.output.csv_name)
-#         else:
-#             continue
+#     if not os.path.exists(params.input.pdb):
+#         continue
+#     if not os.path.exists(params.input.mtz):
 #         continue
 #
-#         params.output.out_dir = os.path.join(out_dir, xtal_name)
-#         if os.path.exists(os.path.join(params.output.out_dir,"exhaustive_search.csv")):
-#             occ, u_iso, fofc = get_minimum_fofc(os.path.join(params.output.out_dir,"exhaustive_search.csv"))
-#             b_fac=u_iso_to_b_fac(u_iso)
+#     params.output.out_dir = os.path.join(out_dir, xtal_name)
+#     params.output.log_dir = os.path.join(out_dir, xtal_name, "logs")
+#     params.exhaustive.output.csv_name = os.path.join(params.output.out_dir, "exhaustive_search.csv")
 #
-#             minima_writer.writerow([xtal_name,occ, b_fac, fofc])
+#     if not qsub:
+#         exhaustive(params=params)
+#         scatter_plot(params.exhaustive.output.csv_name)
+#
+#     if qsub:
+#
+#         # pickle params
+#
+#         with open(os.path.join(out_dir,
+#                                xtal_name,
+#                                '{}_param.pickle'.format(xtal_name)),'wb') as handle:
+#             pickle.dump(params, handle, protocol=pickle.HIGHEST_PROTOCOL)
+#
+#         # write python script
+#
+#         python_script = open(os.path.join(out_dir,
+#                                           xtal_name,
+#                                           "{}_exhaustive.py".format(xtal_name)),'w')
+#         python_script.write("import pickle\n"
+#                             "import sys\n"
+#                             "import os\n"
+#                             "sys.path.append(\"/dls/science/groups/i04-1/elliot-dev/"
+#                             "Work/exhaustive_search\")\n"
+#                             "from exhaustive.exhaustive import run as exhaustive\n"
+#                             "import libtbx.phil\n"
+#                             "out_dir=\"{}\"\n".format(out_dir) +
+#                             "xtal_name=\"{}\"\n".format(xtal_name) +
+#                             "with open(os.path.join(out_dir, " \
+#                             "xtal_name,\'{}_param.pickle\'.format(xtal_name)),'rb') as handle:\n"
+#                             "\tparams = pickle.load(handle)\n"
+#                             "exhaustive(params)")
+#         python_script.close()
+#
+#         # write bash script
+#         bash_script=open(os.path.join(out_dir, xtal_name,
+#                                      "{}_exhaustive.sh".format(xtal_name)),'w')
+#         bash_script.write("source /dls/science/groups/i04-1/software/" \
+#                           "pandda-update/ccp4/ccp4-7.0/setup-scripts/ccp4.setup-sh\n"
+#                           # "export PYTHONPATH=\"${PYTHONPATH}:" \
+#                           # "/dls/science/groups/i04-1/elliot-dev/"
+#                           # "Work/exhaustive_search/exhaustive\"\n"
+#                           "ccp4-python " + os.path.join(out_dir, xtal_name,
+#                                           "{}_exhaustive.py".format(xtal_name)))
+#         bash_script.close()
+#         # submit job
+#         os.system("qsub {}".format(os.path.join(out_dir, xtal_name,xtal_name+"_exhaustive.sh")))
+
+# Get exhaustive search minima fofc
+with open(os.path.join(out_dir,"es_minima.csv"),'wb') as minima_csv:
+
+    minima_writer = csv.writer(minima_csv, delimiter=',')
+
+    for xtal_name in xtals:
+
+        params.output.out_dir = os.path.join(out_dir, xtal_name)
+        params.exhaustive.output.csv_name = os.path.join(params.output.out_dir, "exhaustive_search.csv")
+        if os.path.exists(params.exhaustive.output.csv_name):
+            os.chdir(os.path.join(out_dir, xtal_name))
+            scatter_plot(params.exhaustive.output.csv_name)
+        else:
+            continue
+        continue
+
+        params.output.out_dir = os.path.join(out_dir, xtal_name)
+        if os.path.exists(os.path.join(params.output.out_dir,"exhaustive_search.csv")):
+            occ, u_iso, fofc = get_minimum_fofc(os.path.join(params.output.out_dir,"exhaustive_search.csv"))
+            b_fac=u_iso_to_b_fac(u_iso)
+
+            minima_writer.writerow([xtal_name, occ, b_fac, fofc])
 
 
 
