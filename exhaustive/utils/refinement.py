51b993d918daa07c74e679ac1c7741be34f17ad9
Elliot Nelson <elliot.nelson@dtc.ox.ac.uk>
2018-08-07 14:57:05 +0100

    testing a local copy of quick_refine for phenix refinement

--- exhaustive/utils/refinement.py
+++ exhaustive/utils/refinement.py
@@ -218,7 +218,7 @@ for xtal_name in xtals:
     cmds += "cd {}\n".format(os.path.join(out_dir,xtal_name))
 
     #output.out_prefix =\"{}\"
-    cmds += "/dls/science/groups/i04-1/elliot-dev/Work/exhaustive_search/exhaustive/utils/quick_refine.py " \
+    cmds += "ccp4-python /dls/science/groups/i04-1/elliot-dev/Work/exhaustive_search/exhaustive/utils/quick_refine.py " \
             "{} {} {} output.out_prefix='' params={} program=phenix\n".format(
         input_pdb,
         input_mtz,
