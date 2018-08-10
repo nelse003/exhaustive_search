8450ec0692d85d2ad8146f8b204398e3e5f3e0c5
Elliot Nelson <elliot.nelson@dtc.ox.ac.uk>
2018-08-10 14:24:06 +0100

    Short script to remove residues, to be used with edited params files to test refienemtn with A+B+LIGC rather than A+B+LIGC+LIGD

--- exhaustive/residue_removal.py
+++ exhaustive/residue_removal.py
@@ -5,3 +5,4 @@ remove_residues("/dls/science/groups/i04-1/elliot-dev/Work/exhaustive_search_dat
              "/dls/science/groups/i04-1/elliot-dev/Work/exhaustive_search_data/"
              "covalent_ratios_phenix/NUDT7A-x1906/refine_removed.pdb",
                 [['73','A','C'],['73','A','D'],['1','E','D']])
+
