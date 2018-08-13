1fbc56c68ffc9e2c691e3e1b009f96ae905c9d83
Elliot Nelson <elliot.nelson@dtc.ox.ac.uk>
2018-08-13 18:42:25 +0100

    testing logging

--- exhaustive/exhaustive.py
+++ exhaustive/exhaustive.py
@@ -182,7 +182,7 @@ def get_mean_fofc_over_cart_sites(sites_cart, fofc_map, inputs):
 
         sum_abs_fofc_value += abs(fofc_value)
 
-        logging.debug(site_frac, fofc_value, sum_abs_fofc_value)
+        logging.debug(fofc_value, sum_abs_fofc_value)
 
     mean_abs_fofc_value = sum_abs_fofc_value / len(list(sites_cart))
 
