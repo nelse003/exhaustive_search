4cfed97ccb150610d6826796a1d3fc011208dfc4
nelse003 <nelse003@gmail.com>
2018-08-14 11:30:45 +0100

    exploring whwy xrc_dc is not working with C + D conformers, only changing state of one of them

--- exhaustive/exhaustive.py
+++ exhaustive/exhaustive.py
@@ -391,7 +391,10 @@ def calculate_fofc_occupancy_b_factor(iter_u_iso_occ,
         logging.debug(str(bound_state))
         logging.debug(str(len(sites_frac)))
         print("|||||||||||||||||||||||||||||||||||||||||||||")
+        print(bound_state[0][498])
+        print(bound_state[0][499])
         print(bound_state[0][1809])
+        print(bound_state[0][1810])
         print(bound_state[0])
         print(bound_state[1])
         continue
