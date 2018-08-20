d819710352efe5455609a42731e16652013f6f44
Elliot Nelson <elliot.nelson@dtc.ox.ac.uk>
2018-08-20 09:54:58 +0100

    Add minima check to exhaustive search test

--- exhaustive/test/test_exhaustive_search.py
+++ exhaustive/test/test_exhaustive_search.py
@@ -22,19 +22,20 @@ class TestExhaustiveSearch(unittest.TestCase):
     What mock objects does this need?
     """
     def setUp(self):
+
         self.params = master_phil.extract()
+        self.params.input.pdb = "/dls/labxchem/data/2017/lb18145-49/processing/analysis/" \
+                                "initial_model/NUDT7A-x0299/refine.pdb"
+        self.params.input.mtz = "/dls/labxchem/data/2017/lb18145-49/processing/analysis/" \
+                                "initial_model/NUDT7A-x0299/refine.mtz"
+        self.params.input.xtal_name = "NUDT7A-x0299"
+        self.params.output.out_dir = "/dls/science/groups/i04-1/elliot-dev/Work/" \
+                                     "exhaustive_search_data/tests/"
+        self.params.output.log_dir = "/dls/science/groups/i04-1/elliot-dev/Work/" \
+                                     "exhaustive_search_data/tests/logs"
 
     def test_exhaustive_search(self):
 
-        self.params.input.pdb = "/dls/labxchem/data/2017/lb18145-49/processing/analysis/" \
-                           "initial_model/NUDT7A-x0299/refine.pdb"
-        self.params.input.mtz = "/dls/labxchem/data/2017/lb18145-49/processing/analysis/" \
-                           "initial_model/NUDT7A-x0299/refine.mtz"
-        self.params.input.xtal_name = "NUDT7A-x0299"
-        self.params.output.out_dir = "/dls/science/groups/i04-1/elliot-dev/Work/" \
-                                "exhaustive_search_data/tests/"
-        self.params.output.log_dir = "/dls/science/groups/i04-1/elliot-dev/Work/" \
-                                "exhaustive_search_data/tests/logs"
         self.params.exhaustive.output.csv_name = os.path.join(self.params.output.out_dir, "test.csv")
         exhaustive(self.params)
         bound_occ, u_iso, fofc = get_minimum_fofc(self.params.exhaustive.output.csv_name)
@@ -44,11 +45,21 @@ class TestExhaustiveSearch(unittest.TestCase):
     def test_convex_hull_exhaustive_search(self):
         self.assertEqual(True, False)
 
-    def test_conex_hull_buffer_exhaustive_search(self):
+        self.params.exhaustive.output.csv_name = os.path.join(self.params.output.out_dir, "test.csv")
+        exhaustive(self.params)
+        bound_occ, u_iso, fofc = get_minimum_fofc(self.params.exhaustive.output.csv_name)
+        self.assertAlmostEqual(0.7,bound_occ)
+        self.assertAlmostEqual(0.45,u_iso)
+
+    def test_convex_hull_buffer_exhaustive_search(self):
         self.assertEqual(True, False)
 
     def test_convex_hull_buffer_ignore_nearest(self):
         self.assertEqual(True, False)
+
+    def test_step_size(self):
+        self.assertEqual(True, False)
+
 
 # TODO Write TestOccupancyCartPoints
 class TestOccupancyCartPoints(unittest.TestCase):
