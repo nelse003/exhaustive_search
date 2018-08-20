a924e701f8c3494415b9b269acc26c1133172ae8
Elliot Nelson <elliot.nelson@dtc.ox.ac.uk>
2018-08-20 09:01:46 +0100

    Add exhaustive search test code

--- exhaustive/test/test_exhaustive_search.py
+++ exhaustive/test/test_exhaustive_search.py
@@ -1,5 +1,6 @@
 import unittest
-
+from exhaustive.exhaustive.phil import master_phil
+from exhaustive.exhaustive.exhaustive import run as exhaustive
 # TODO Write TestComputeMaps
 class TestComputeMaps(unittest.TestCase):
     """
@@ -17,10 +18,30 @@ class TestExhaustiveSearch(unittest.TestCase):
     
     What mock objects does this need?
     """
-
     def test_exhaustive_search(self):
+
+        params = master_phil.extract()
+        params.input.pdb = "/dls/labxchem/data/2017/lb18145-49/processing/analysis/" \
+                           "initial_model/NUDT7A-x0299/refine.pdb"
+        params.input.mtz = "/dls/labxchem/data/2017/lb18145-49/processing/analysis/" \
+                           "initial_model/NUDT7A-x0299/refine.mtz"
+        params.input.xtal_name = "NUDT7A-x0299"
+        params.output.out_dir = "/dls/science/groups/i04-1/elliot-dev/Work/" \
+                                "exhaustive_search_data/tests/"
+        params.output.log_dir = "/dls/science/groups/i04-1/elliot-dev/Work/" \
+                                "exhaustive_search_data/tests/logs"
+        params.exhaustive.output.csv_name = os.path.join(params.output.out_dir, "test.csv")
+
+        exhaustive(params)
+
+    def test_convex_hull_exhaustive_search(self):
         self.assertEqual(True, False)
 
+    def test_conex_hull_buffer_exhaustive_search(self):
+        self.assertEqual(True, False)
+
+    def test_convex_hull_buffer_ignore_nearest(self):
+        self.assertEqual(True, False)
 
 # TODO Write TestOccupancyCartPoints
 class TestOccupancyCartPoints(unittest.TestCase):
