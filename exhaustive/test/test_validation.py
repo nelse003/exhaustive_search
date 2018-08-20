b60e5b0b04eadc888bce6ff97e9323cef8c633fe
Elliot Nelson <elliot.nelson@dtc.ox.ac.uk>
2018-08-20 12:04:28 +0100

    trying to run validation script

--- exhaustive/test/test_validation.py
+++ exhaustive/test/test_validation.py
@@ -54,8 +54,8 @@ class TestExhaustiveSearch(unittest.TestCase):
 
     def test_validation(self):
         validate(self.params)
-        self.check_validate_result(self)
-        self.check_validate_ouput_files(self)
+        self.check_validate_result()
+        self.check_validate_ouput_files()
 
 
     def check_validate_result(self):
