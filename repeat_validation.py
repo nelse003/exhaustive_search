4339bcaf6dd4fef855779990ac298cd80c4a8698
Elliot Nelson <elliot.nelson@dtc.ox.ac.uk>
2018-08-29 15:27:38 +0100

    Writing a way to loop over many different folders

--- repeat_validation.py
+++ repeat_validation.py
@@ -29,7 +29,7 @@ compound_dirs = [compound_dir for compound_dir in os.listdir(loop_dir)
 for compound_dir in compound_dirs:
 
     xtal_dirs = [xtal_dir for xtal_dir in os.listdir(compound_dir)
-                 if os.path.isdir(compound_dir, xtal_dir))]
+                 if os.path.isdir(os.path.join(compound_dir, xtal_dir))]
 
     for xtal_dir in xtal_dir:
 
