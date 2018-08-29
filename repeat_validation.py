10c100c264c5eb98bb2579bb1e6aa4c0a7df2a1c
Elliot Nelson <elliot.nelson@dtc.ox.ac.uk>
2018-08-29 15:26:44 +0100

    Writing a way to loop over many different folders

--- repeat_validation.py
+++ repeat_validation.py
@@ -23,9 +23,16 @@ prefix = "NUDT22A-x"
 
 loop_dir= "/dls/science/groups/i04-1/elliot-dev/Work/exhaustive_search_data/repeat_soaks/2018-05-28/NUDT22_from_occ_group_with_refinement/"
 
-for compound_dir in os.listdir(loop_dir):
-    print(compound_dir)
-    for xtal_dir in os.listdir(os.path.join(loop_dir,compound_dir)):
+compound_dirs = [compound_dir for compound_dir in os.listdir(loop_dir)
+                 if os.path.isdir(os.path.join(loop_dir,compound_dir))]
+
+for compound_dir in compound_dirs:
+
+    xtal_dirs = [xtal_dir for xtal_dir in os.listdir(compound_dir)
+                 if os.path.isdir(compound_dir, xtal_dir))]
+
+    for xtal_dir in xtal_dir:
+
         print(xtal_dir)
 
 exit()
