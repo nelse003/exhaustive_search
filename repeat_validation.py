1828ffe5bce197efd40872869860b7eda36e866a
Elliot Nelson <elliot.nelson@dtc.ox.ac.uk>
2018-08-29 17:59:08 +0100

    validation across many repeated datasets

--- repeat_validation.py
+++ repeat_validation.py
@@ -8,19 +8,6 @@ from giant.jiffies.split_conformations import run as split_conformations
 
 params =  master_phil.extract()
 
-# start_xtal_num = 1905
-# end_xtal_num = 1915
-# in_dir = "/dls/science/groups/i04-1/elliot-dev/Work/exhaustive_search_data/covalent_ratios"
-# out_dir = "/dls/science/groups/i04-1/elliot-dev/Work/exhaustive_search_data/validation_covalent_ratios"
-# prefix = "NUDT7A-x"
-
-# start_xtal_num = 909
-# end_xtal_num = 937
-# in_dir = "/dls/science/groups/i04-1/elliot-dev/Work/exhaustive_search_data/repeat_soaks/2018-05-28/NUDT22_from_occ_group_with_refinement/FMOPL000622a_DSPL"
-# out_dir = "/dls/science/groups/i04-1/elliot-dev/Work/exhaustive_search_data/validation_NUDT22/FMOPL000622a_DSPL"
-# prefix = "NUDT22A-x"
-
-
 out_dir =  "/dls/science/groups/i04-1/elliot-dev/Work/exhaustive_search_data/validation_NUDT22/"
 loop_dir= "/dls/science/groups/i04-1/elliot-dev/Work/exhaustive_search_data/repeat_soaks/2018-05-28/NUDT22_from_occ_group_with_refinement/"
 
@@ -68,22 +55,6 @@ params.exhaustive.options.step = 0.05
 params.settings.processes = 20
 params.validate.options.set_b = 40
 
-# # # copy data to new folder
-#
-# if not os.path.exists("/dls/science/groups/i04-1/elliot-dev/Work/exhaustive_search_data/validation_NUDT22/"):
-#     os.mkdir("/dls/science/groups/i04-1/elliot-dev/Work/exhaustive_search_data/validation_NUDT22/")
-#
-# if not os.path.exists(out_dir):
-#     os.mkdir(out_dir)
-#     #os.system('cp -a {}/. {}'.format(in_dir,out_dir))
-#
-# xtals = ["NUDT22A-x0182"]
-# for num in range(start_xtal_num, end_xtal_num + 1):
-#     xtal_name = prefix + "{0:0>4}".format(num)
-#     xtals.append(xtal_name)
-#
-# for xtal_name in xtals:
-
 for dataset in datasets:
 
     print(dataset)
@@ -91,25 +62,6 @@ for dataset in datasets:
     (params.input.xtal_name, params.input.in_path,  params.input.pdb, params.input.mtz, params.output.out_dir) = dataset
     params.validate.input.base_mtz = params.input.mtz
     params.output.log_dir = os.path.join(params.output.out_dir, "logs")
-
-
-    # params.input.xtal_name = xtal_name
-    # params.input.pdb = os.path.join(os.path.join(in_dir, xtal_name, "refine.pdb"))
-    # params.input.mtz = os.path.join(os.path.join(in_dir, xtal_name, "refine.mtz"))
-    #
-    # params.validate.input.base_mtz = os.path.join(os.path.join(in_dir,
-    #                                                            xtal_name,
-    #                                                            "refine.mtz"))
-    # print(params.input.pdb)
-    #
-    # if not os.path.exists(params.input.pdb):
-    #     continue
-    # if not os.path.exists(params.input.mtz):
-    #     continue
-
-    # params.output.out_dir = os.path.join(out_dir, xtal_name)
-    # params.output.log_dir = os.path.join(out_dir, xtal_name, "logs")
-    #params.exhaustive.output.csv_name = os.path.join(params.output.out_dir, "exhaustive_search.csv")
 
     params.validate.input.ground_state_pdb_path = os.path.join(
         params.input.in_path, "refine.output.ground-state.pdb")
@@ -139,50 +91,38 @@ for dataset in datasets:
                 if not item.startswith("refine"):
                     os.remove(os.path.join(params.output.out_dir, item))
 
-    # Loop over set B. Fails at setB =100
 
-    # for set_b in np.arange(20,120,5):
-    #     params.validate.options.set_b = set_b
-    #     params.output.out_dir = os.path.join(os.path.join(out_dir, xtal_name, "set_b_{}".format(str(set_b).replace(".","_"))))
-    #
-    #     if os.path.exists(params.output.out_dir):
-    #
-    #         for item in os.listdir(params.output.out_dir):
-    #             if item.endswith(".mtz"):
-    #                 if not item.startswith("refine"):
-    #                     os.remove(os.path.join(params.output.out_dir, item))
-    #
-    #     validate(params)
-
-    # params.output.out_dir = os.path.join(os.path.join(out_dir, xtal_name, "per_residue"))
-    # if os.path.exists(params.output.out_dir):
-    #     for item in os.listdir(params.output.out_dir):
-    #         if item.endswith(".mtz"):
-    #             if not item.startswith("refine"):
-    #                 os.remove(os.path.join(params.output.out_dir, item))
-    #params.exhaustive.options.per_residue = True
-
-    validate(params)
+    repeat_validate(params)
     exit()
 
-    # Atom selection isnt working? Something to do with merging in validate?
+def repeat_validate(params):
 
-    # params.exhaustive.options.convex_hull = False
-    # atom_points_sel_string = "(chain B and altid C and resid 1) or (chain B and altid D resid 1)"
-    # params.output.out_dir = os.path.join(
-    #     os.path.join(out_dir, xtal_name, "lig_atoms"))
-    # params.exhaustive.options.ligand_atom_points = True
-    # validate(params)
-    #
-    # params.exhaustive.options.ligand_atom_points = False
-    # params.exhaustive.options.ligand_grid_points = True
-    # params.output.out_dir = os.path.join(
-    #     os.path.join(out_dir, xtal_name, "lig_grid"))
-    # validate(params)
+    print(atom_points_from_sel_string(params,"resname LIG"))
+    exit()
 
-    params.validate.options.set_b = 40
+    # Ligand grid (by convex hull of ligand atoms)
+    params.exhaustive.options.ligand_grid_points = True
+    validate(params)
+    # Add plotting of residue selection
 
-    for buffer in np.arange(0, 3, 0.5):
+    # Reset
+    params.exhaustive.options.ligand_grid_points = False
+
+
+    # Per residue selection of atoms
+    params.exhaustive.options.per_residue = True
+    params.output.out_dir = os.path.join(params.output.out_dir, "per_residue")
+    if os.path.exists(params.output.out_dir):
+        for item in os.listdir(params.output.out_dir):
+            if item.endswith(".mtz"):
+                if not item.startswith("refine"):
+                    os.remove(os.path.join(params.output.out_dir, item))
+    validate(params)
+    # Add plotting of residue selection
+
+
+    # Buffer range (convex hull around occupancy group)
+    for buffer in np.arange(0, 2, 0.5):
         params.exhaustive.options.convex_hull_ignore_nearest = False
         params.output.out_dir = os.path.join(os.path.join(out_dir, xtal_name, "test_convex_hull_buffer_{}".format(str(buffer).replace(".","_"))))
         params.exhaustive.options.convex_hull=True
@@ -197,30 +137,6 @@ for dataset in datasets:
 
         validate(params)
 
-        params.exhaustive.options.convex_hull_ignore_nearest =True
-        params.output.out_dir = os.path.join(
-            os.path.join(out_dir, xtal_name, "convex_hull_ignore_nearest_buffer_{}".format(str(buffer).replace(".", "_"))))
 
-        if os.path.exists(params.output.out_dir):
-
-            for item in os.listdir(params.output.out_dir):
-                if item.endswith(".mtz"):
-                    if not item.startswith("refine"):
-                        os.remove(os.path.join(params.output.out_dir, item))
-
-        validate(params)
-
-        params.output.out_dir = os.path.join(
-            os.path.join(out_dir, xtal_name, "buffer_{}".format(str(buffer).replace(".", "_"))))
-        params.exhaustive.options.convex_hull = False
-
-        if os.path.exists(params.output.out_dir):
-
-            for item in os.listdir(params.output.out_dir):
-                if item.endswith(".mtz"):
-                    if not item.startswith("refine"):
-                        os.remove(os.path.join(params.output.out_dir, item))
-
-        validate(params)
 
 
