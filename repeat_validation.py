17dc6988d75966e927f6044919e3dffc04e9a192
Elliot Nelson <elliot.nelson@dtc.ox.ac.uk>
2018-08-29 16:34:44 +0100

    Writing a way to loop over many different folders

--- repeat_validation.py
+++ repeat_validation.py
@@ -76,6 +76,9 @@ params.settings.processes = 20
 
 for dataset in datasets:
 
+    print(dataset)
+    exit()
+
     (params.input.xtal_name, params.input.in_path,  params.input.pdb, params.input.mtz, params.output.out_dir) = dataset
     params.validate.input.base_mtz = params.validate.input.base_mtz
     params.output.log_dir = os.path.join(params.output.out_dir, "logs")
