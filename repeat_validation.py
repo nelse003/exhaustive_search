f75d440712cb039574b4f1eead5c89339f498a88
Elliot Nelson <elliot.nelson@dtc.ox.ac.uk>
2018-08-30 16:05:41 +0100

    qsub developments for repeat validate

--- repeat_validation.py
+++ repeat_validation.py
@@ -12,142 +12,7 @@ from exhaustive.exhaustive.utils.utils import process_validation_csvs, u_iso_to_
 from exhaustive.exhaustive.plotting.plot import plot_protein_and_selection
 from exhaustive.exhaustive.utils.select_atoms import process_refined_pdb_bound_ground_states
 
-def repeat_validate(params):
-
-    # Issue: Can't do selection of lig atoms for each case, can we do it by LIG naming
-
-    # Ligand grid (by convex hull of ligand atoms)
-    params.exhaustive.options.ligand_grid_points = True
-    params.output.out_dir = os.path.join(params.output.out_dir, "lig_grid")
-    validate(params)
-    summary_validation(params)
-
-    # Add plotting of residue selection
-    atom_points = atom_points_from_sel_string(pdb=params.input.pdb,
-                                              selection_string=
-                                              params.exhaustive.options.atom_points_sel_string)
-
-    lig_grid_points = convex_hull_grid_points(atom_points,params)
-
-    plot_protein_and_selection(pdb=params.input.pdb,
-                               atom_points=lig_grid_points,
-                               plot_filename=os.path.join(
-                                   params.output.out_dir,
-                                   "lig_grid_points.png"),
-                               params=params)
-
-    # Reset
-    params.exhaustive.options.ligand_grid_points = False
-    params.output.out_dir = os.path.split(params.output.out_dir)[0]
-
-    # Per residue selection of atoms
-    params.exhaustive.options.per_residue = True
-    params.output.out_dir = os.path.join(params.output.out_dir, "per_residue")
-    if os.path.exists(params.output.out_dir):
-        for item in os.listdir(params.output.out_dir):
-            if item.endswith(".mtz"):
-                if not item.startswith("refine"):
-                    os.remove(os.path.join(params.output.out_dir, item))
-    validate(params)
-    summary_validation(params)
-
-    # Add plotting of residue selection
-    bound_states, \
-    ground_states = process_refined_pdb_bound_ground_states(pdb=params.input.pdb,
-                                                            params=params)
-
-    per_residue_points = convex_hull_per_residue(pdb=params.input.pdb,
-                                          bound_states=bound_states,
-                                          ground_states=ground_states,
-                                          params=params)
-
-    plot_protein_and_selection(pdb=params.input.pdb,
-                               atom_points=per_residue_points,
-                               plot_filename=os.path.join(
-                                   params.output.out_dir,
-                                   "per_residue_points.png"),
-                               params=params)
-
-    #Reset
-    params.exhaustive.options.per_residue = False
-    params.output.out_dir = os.path.split(params.output.out_dir)[0]
-    print(params.output.out_dir)
-
-    # Buffer range (convex hull around occupancy group)
-    for buffer in np.arange(0, 2, 0.5):
-        params.exhaustive.options.convex_hull_ignore_nearest = False
-        params.output.out_dir = os.path.join(params.output.out_dir,
-                                             "convex_hull_buffer_{}".format(str(buffer).replace(".","_")))
-        params.exhaustive.options.convex_hull=True
-        params.exhaustive.options.buffer=buffer
-
-        if os.path.exists(params.output.out_dir):
-
-            for item in os.listdir(params.output.out_dir):
-                if item.endswith(".mtz"):
-                    if not item.startswith("refine"):
-                        os.remove(os.path.join(params.output.out_dir, item))
-
-        validate(params)
-        summary_validation(params)
-
-        buffered_points = convex_hull_from_states(pdb=params.input.pdb,
-                                              bound_states=bound_states,
-                                              ground_states=ground_states,
-                                              params=params)
-
-        plot_protein_and_selection(pdb=params.input.pdb,
-                               atom_points=buffered_points,
-                               plot_filename=os.path.join(
-                                   params.output.out_dir,
-                                   "buffer_{}_points.png".format(
-                                       str(params.validate.options.set_b).replace('.','_'))),
-                               params=params)
-
-
-def summary_validation(params):
-
-    min_fofcs, min_occs, min_b_facs, fofcs, occs, b_facs = \
-        process_validation_csvs(params.validate.options.start_simul_occ,
-                     params.validate.options.end_simul_occ,
-                     step=params.validate.options.step_simulation,
-                     set_b=params.validate.options.set_b,
-                     out_dir=params.output.out_dir,
-                     params=params)
-
-    occ_delta = np.abs(np.array(min_occs) - np.array(occs))
-    b_delta = np.abs(np.array(min_b_facs) - np.array(b_facs))
-
-    normalised_min_b_fac = (np.array(min_b_facs)
-                            - u_iso_to_b_fac(
-        params.exhaustive.options.lower_u_iso))/(u_iso_to_b_fac(
-        params.exhaustive.options.upper_u_iso)-u_iso_to_b_fac(
-        params.exhaustive.options.lower_u_iso))
-
-    normalised_b_fac = (np.array(b_facs)
-                            - u_iso_to_b_fac(
-        params.exhaustive.options.lower_u_iso))/(u_iso_to_b_fac(
-        params.exhaustive.options.upper_u_iso)-u_iso_to_b_fac(
-        params.exhaustive.options.lower_u_iso))
-
-    norm_b_delta = np.abs(normalised_min_b_fac - normalised_b_fac)
-
-    mean_occ_delta =  np.mean(occ_delta)
-    mean_b_delta =  np.mean(b_delta)
-    dst = np.sqrt(occ_delta**2 + norm_b_delta**2 )
-    mean_dst = np.mean(dst)
-
-    print(os.path.join(params.output.out_dir, "validation_summary.csv"))
-
-    with open(os.path.join(params.output.out_dir,
-                           "validation_summary.csv"),
-              'wb') as validation_csv:
-
-        validation_writer = csv.writer(validation_csv, delimiter=',')
-        validation_writer.writerow(["mean_occ_delta", "mean_b_delta", "mean_dst"])
-        validation_writer.writerow([mean_occ_delta,mean_b_delta,mean_dst])
-
-
+from exhaustive.validation.repeat_validate import repeat_validate
 
 params =  master_phil.extract()
 
@@ -245,12 +110,12 @@ for dataset in datasets:
             python_file.write('import os, sys\n')
             python_file.write('scriptpath=\'/dls/science/groups/i04-1/elliot-dev/Work/exhaustive_search\'\n')
             python_file.write('sys.path.append(os.path.abspath(scriptpath))\n')
-            python_file.write('from repeat_validation import repeat_validate\n')
+            python_file.write('from repeat_validation import repeat_validate.py\n')
             python_file.write('master_phil=parse(os.path.join(params.output.out_dir, "params.txt"))\n')
             python_file.write('params =  master_phil.extract()\n')
             python_file.write('print("BBBBBBBBBBBBBBBBB")\n')
             python_file.write('print(params.output.out_dir)\n')
-            #python_file.write('repeat_validate(params)\n')
+            #python_file.write('repeat_validate.py(params)\n')
     else:
         repeat_validate(params)
 
