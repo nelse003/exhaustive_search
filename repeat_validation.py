3e66d5e795986914ca859e690e5e9f30e79d3e2b
Elliot Nelson <elliot.nelson@dtc.ox.ac.uk>
2018-08-30 15:26:37 +0100

    qsub developments for repeat validate

--- repeat_validation.py
+++ repeat_validation.py
@@ -236,12 +236,11 @@ for dataset in datasets:
     params.validate.options.repeat_validate_qsub = True
 
     if params.validate.options.repeat_validate_qsub:
-        modified_phil = master_phil.format(python_object=params)
 
-        print(modified_phil)
+        print(params)
 
         with open(os.path.join(params.output.out_dir, "params.txt"),'w+') as param_file:
-            param_file.write(modified_phil)
+            param_file.write(params)
         with open(os.path.join(params.output.out_dir, "run_repeat_validation.py"),'w+') as python_file:
             python_file.write('from repeat_validation import repeat_validate\n')
             python_file.write('master_phil=parse(os.path.join(params.output.out_dir, "params.txt"))\n')
