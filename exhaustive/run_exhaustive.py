89480042d170f59c7720df470ac8c9ecff5a9b27
Elliot Nelson <elliot.nelson@dtc.ox.ac.uk>
2018-09-10 16:22:11 +0100

    testing phil file issues

--- exhaustive/run_exhaustive.py
+++ exhaustive/run_exhaustive.py
@@ -4,7 +4,7 @@ import os
 from exhaustive.exhaustive import run as exhaustive
 from exhaustive.plotting.plot import scatter_plot
 from exhaustive.utils.utils import get_minimum_fofc, u_iso_to_b_fac
-from phil import master_phil
+from ..phil import master_phil
 
 params =  master_phil.extract()
 
