ac203314abf3080a8f2eb6bb7e663fc85f209530
Elliot Nelson <elliot.nelson@dtc.ox.ac.uk>
2018-09-10 15:51:55 +0100

    running exhaustive on single dataset for testing with logs

--- exhaustive/run_exhaustive.py
+++ exhaustive/run_exhaustive.py
@@ -1,7 +1,7 @@
 import csv
 import os
 
-from exhaustive.exhaustive.plotting.plot import scatter_plot
+from exhaustive.plotting.plot import scatter_plot
 from exhaustive.exhaustive.utils import get_minimum_fofc, u_iso_to_b_fac
 from phil import master_phil
 
