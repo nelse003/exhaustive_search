82417f70c8aa839ee488ce31f7426d6d22ceebe2
Elliot Nelson <elliot.nelson@dtc.ox.ac.uk>
2018-09-11 10:21:24 +0100

    looking at occupancy group to state selection method.

--- exhaustive/validation/validation.py
+++ exhaustive/validation/validation.py
@@ -344,7 +344,6 @@ def occ_loop_merge_confs_simulate(params):
             else:
                 logging.info("Running exhaustive search locally")
                 exhaustive(params=params)
-            exit()
         else:
             logging.info("Skipping exhaustive search")
 
