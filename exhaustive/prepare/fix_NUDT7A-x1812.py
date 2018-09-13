9ce3b4f845fcde13a96b53ba241866c83569b495
Elliot Nelson <elliot.nelson@dtc.ox.ac.uk>
2018-09-13 16:08:51 +0100

    The refined structure of NUDT7A-x1812/refine.pdb is incorrect (cif error?). pull from ensemble model for run exhaustive

--- exhaustive/prepare/fix_NUDT7A-x1812.py
+++ exhaustive/prepare/fix_NUDT7A-x1812.py
@@ -4,5 +4,5 @@ pandda_model = "/dls/labxchem/data/2017/lb18145-49/processing/analysis/initial_m
 os.mkdir("/dls/science/groups/i04-1/elliot-dev/Work/exhaustive_search_data/NUDT7A-x1812-new-test")
 os.chdir("/dls/science/groups/i04-1/elliot-dev/Work/exhaustive_search_data/NUDT7A-x1812-new-test")
 
-os.system("giant.merge_conformations major={} minor={}".format(pandda_input,pandda_model)
+os.system("giant.merge_conformations major={} minor={}".format(pandda_input,pandda_model))
 
