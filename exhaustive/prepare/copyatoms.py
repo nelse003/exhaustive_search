16647a6f2b051eff1bfa08c55e9b2deb6d5987dc
Elliot Nelson <elliot.nelson@dtc.ox.ac.uk>
2018-09-13 16:15:29 +0100

    The refined structure of NUDT7A-x1812/refine.pdb is incorrect (cif error?). pull from pandda model

--- exhaustive/prepare/copyatoms.py
+++ exhaustive/prepare/copyatoms.py
@@ -204,9 +204,9 @@ copy_params = copy_phil.extract()
 
 copy_params.input.path = "/dls/labxchem/data/2017/lb18145-49/processing/analysis/initial_model"
 copy_params.input.prefix = 'NUDT7A-x'
-copy_params.input.start_xtal_number = 6192
-copy_params.input.end_xtal_number = 6251
-copy_params.input.base_pdb = "/dls/labxchem/data/2017/lb18145-49/processing/analysis/initial_model/NUDT7A-x1812/NUDT7A-x1812-ensemble-model.pdb"
+copy_params.input.start_xtal_number = 1812
+copy_params.input.end_xtal_number = 1812
+copy_params.input.base_pdb = "/dls/labxchem/data/2017/lb18145-49/processing/analysis/initial_model/NUDT7A-x1812/NUDT7A-x1812-pandda-model.pdb"
 copy_params.input.atoms_new = [['E','1']]
 copy_params.input.cif = "/dls/science/groups/i04-1/elliot-dev/Work/" \
                         "exhaustive_search_data/NUDT7_covalent" \
@@ -215,7 +215,7 @@ copy_params.input.link_record_list =["LINKR        C  CLIG E   1                
                                      "LINKR        D  CLIG E   1                 SG ACYS A  73                LIG-CYS\n"]
 
 copy_params.output.out_dir = "/dls/science/groups/i04-1/elliot-dev/Work/" \
-                   "exhaustive_search_data/covalent_ratios_dose"
+                   "exhaustive_search_data/covalent_1812_test"
 
 copy_params.settings.overwrite = True
 
