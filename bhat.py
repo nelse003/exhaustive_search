f6840c88f14c439dcf847bf1d840cfa47f5d817f
Elliot Nelson <elliot.nelson@dtc.ox.ac.uk>
2018-09-17 17:03:58 +0100

    Adding a test file for looking at plot in 1989 Bhat paper

---
+++ bhat.py
@@ -0,0 +1,24 @@
+from cctbx import maptbx
+from iotbx import reflection_file_utils
+from phil import master_phil
+
+params = master_phil.extract()
+params.input.xtal_name = "FALZA-x0085"
+params.input.in_path = os.path.join(os.path.realpath(
+    "./exhaustive/test/resources"), params.input.xtal_name)
+params.validate.input.base_mtz = os.path.join(params.input.in_path,
+                                                   "FALZA-x0085.free.mtz")
+params.input.mtz = os.path.join(params.input.in_path,
+                                     "FALZA-x0085.free.mtz")
+params.input.pdb = os.path.join(params.input.in_path, "refine.pdb")
+
+args = [params.input.pdb, params.input.mtz]
+inputs = mmtbx.utils.process_command_line_args(args=args)
+rfs = reflection_file_utils.reflection_file_server(
+    crystal_symmetry=inputs.crystal_symmetry,
+    force_symmetry=True,
+    reflection_files=inputs.reflection_files,
+    err=StringIO())
+xrs = ph.extract_xray_structure(
+    crystal_symmetry=inputs.crystal_symmetry)
+xrs.show_summary()
