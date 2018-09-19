57de4401acef6c48b72e68443722703cd7a04cfb
Elliot Nelson <elliot.nelson@dtc.ox.ac.uk>
2018-09-19 17:02:44 +0100

    Add folder looping for falza run. Need to make this more general

--- run_exhaustive.py
+++ run_exhaustive.py
@@ -47,6 +47,18 @@ params =  master_phil.extract()
 # prefix = "NUDT7A-x"
 qsub = False
 
+# # copy data to new folder
+
+# if not os.path.exists(out_dir):
+#     os.mkdir(out_dir)
+#     os.system('cp -a {}/. {}'.format(in_dir,out_dir))
+
+# Single dataset
+# params.exhaustive.output.csv_name = os.path.join(params.output.out_dir, "exhaustive_search.csv")
+# exhaustive(params=params)
+# scatter_plot(params.exhaustive.output.csv_name)
+# plot_protein_region(params)
+
 #FALZA exhaustive search
 
 out_dir =  "/dls/science/groups/i04-1/elliot-dev/Work/exhaustive_search_data/FALZA_exhaus_18_09_18/"
@@ -65,17 +77,6 @@ params.exhaustive.options.per_residue = True
 params.exhaustive.options.ligand_grid_points = False
 params.exhaustive.options.generate_mtz = True
 
-# # copy data to new folder
-
-# if not os.path.exists(out_dir):
-#     os.mkdir(out_dir)
-#     os.system('cp -a {}/. {}'.format(in_dir,out_dir))
-
-# Single dataset
-# params.exhaustive.output.csv_name = os.path.join(params.output.out_dir, "exhaustive_search.csv")
-# exhaustive(params=params)
-# scatter_plot(params.exhaustive.output.csv_name)
-# plot_protein_region(params)
 
 xtals = []
 # for num in range(start_xtal_num, end_xtal_num + 1):
@@ -84,69 +85,98 @@ xtals = []
 #
 # print(xtals)
 
-for xtal_name in xtals:
+xtal_dirs = [os.path.join(loop_dir,xtal_dir) for xtal_dir in os.listdir(loop_dir)
+             if os.path.isdir(os.path.join(loop_dir, xtal_dir))]
+for xtal_dir in xtal_dirs:
 
-    params.input.xtal_name = xtal_name
-    params.input.pdb = os.path.join(os.path.join(in_dir, xtal_name, "refine.pdb"))
-    params.input.mtz = os.path.join(os.path.join(in_dir, xtal_name, "refine.mtz"))
+    xtal_name = os.path.basename(xtal_dir)
 
-    if not os.path.exists(params.input.pdb):
-        print("input pdb doesn't exist: {}".format(params.input.pdb))
-        continue
-    if not os.path.exists(params.input.mtz):
-        print("input mtz doesn't exsit: {}".format(params.input.mtz))
-        continue
+    if xtal_name in xtals:
 
-    params.output.out_dir = os.path.join(out_dir, xtal_name)
-    params.output.log_dir = os.path.join(out_dir, xtal_name, "logs")
-    params.exhaustive.output.csv_name = os.path.join(params.output.out_dir, "exhaustive_search.csv")
+        compounds = list_files(xtal_dir,"cif")
+        compound_name = (list_files(xtal_dir,"cif")[0]).split(".")[0]
 
-    if not qsub:
+        params.input.pdb = os.path.join(xtal_dir,"refine.pdb")
+        params.input.mtz = os.path.join(xtal_dir,"refine.mtz")
+        params.output.out_dir = os.path.join(out_dir, compound_name, xtal_name)
+
+        if not os.path.exists(params.input.pdb):
+            print("input pdb doesn't exist: {}".format(params.input.pdb))
+            continue
+        if not os.path.exists(params.input.mtz):
+            print("input mtz doesn't exsit: {}".format(params.input.mtz))
+            continue
+
+        params.exhaustive.output.csv_name = os.path.join(params.output.out_dir, "exhaustive_search.csv")
+
         exhaustive(params=params)
         scatter_plot(params.exhaustive.output.csv_name)
 
-    if qsub:
 
-        # pickle params
 
-        with open(os.path.join(out_dir,
-                               xtal_name,
-                               '{}_param.pickle'.format(xtal_name)),'wb') as handle:
-            pickle.dump(params, handle, protocol=pickle.HIGHEST_PROTOCOL)
-
-        # write python script
-
-        python_script = open(os.path.join(out_dir,
-                                          xtal_name,
-                                          "{}_exhaustive.py".format(xtal_name)),'w')
-        python_script.write("import pickle\n"
-                            "import sys\n"
-                            "import os\n"
-                            "sys.path.append(\"/dls/science/groups/i04-1/elliot-dev/"
-                            "Work/exhaustive_search\")\n"
-                            "from exhaustive.exhaustive import run as exhaustive\n"
-                            "import libtbx.phil\n"
-                            "out_dir=\"{}\"\n".format(out_dir) +
-                            "xtal_name=\"{}\"\n".format(xtal_name) +
-                            "with open(os.path.join(out_dir, " \
-                            "xtal_name,\'{}_param.pickle\'.format(xtal_name)),'rb') as handle:\n"
-                            "\tparams = pickle.load(handle)\n"
-                            "exhaustive(params)")
-        python_script.close()
-
-        # write bash script
-        bash_script=open(os.path.join(out_dir, xtal_name,
-                                     "{}_exhaustive.sh".format(xtal_name)),'w')
-        bash_script.write("source /dls/science/groups/i04-1/software/" \
-                          "pandda-update/ccp4/ccp4-7.0/setup-scripts/ccp4.setup-sh\n"
-                          # "export PYTHONPATH=\"${PYTHONPATH}:" \
-                          # "/dls/science/groups/i04-1/elliot-dev/"
-                          # "Work/exhaustive_search/exhaustive\"\n"
-                          "ccp4-python " + os.path.join(out_dir, xtal_name,
-                                          "{}_exhaustive.py".format(xtal_name)))
-        bash_script.close()
-        # submit job
-        os.system("qsub {}".format(os.path.join(out_dir, xtal_name,xtal_name+"_exhaustive.sh")))
+# for xtal_name in xtals:
+#
+#     params.input.xtal_name = xtal_name
+#     params.input.pdb = os.path.join(os.path.join(in_dir, xtal_name, "refine.pdb"))
+#     params.input.mtz = os.path.join(os.path.join(in_dir, xtal_name, "refine.mtz"))
+#
+#     if not os.path.exists(params.input.pdb):
+#         print("input pdb doesn't exist: {}".format(params.input.pdb))
+#         continue
+#     if not os.path.exists(params.input.mtz):
+#         print("input mtz doesn't exsit: {}".format(params.input.mtz))
+#         continue
+#
+#     params.output.out_dir = os.path.join(out_dir, xtal_name)
+#     params.output.log_dir = os.path.join(out_dir, xtal_name, "logs")
+#     params.exhaustive.output.csv_name = os.path.join(params.output.out_dir, "exhaustive_search.csv")
+#
+#     if not qsub:
+#         exhaustive(params=params)
+#         scatter_plot(params.exhaustive.output.csv_name)
+#
+#     if qsub:
+#
+#         # pickle params
+#
+#         with open(os.path.join(out_dir,
+#                                xtal_name,
+#                                '{}_param.pickle'.format(xtal_name)),'wb') as handle:
+#             pickle.dump(params, handle, protocol=pickle.HIGHEST_PROTOCOL)
+#
+#         # write python script
+#
+#         python_script = open(os.path.join(out_dir,
+#                                           xtal_name,
+#                                           "{}_exhaustive.py".format(xtal_name)),'w')
+#         python_script.write("import pickle\n"
+#                             "import sys\n"
+#                             "import os\n"
+#                             "sys.path.append(\"/dls/science/groups/i04-1/elliot-dev/"
+#                             "Work/exhaustive_search\")\n"
+#                             "from exhaustive.exhaustive import run as exhaustive\n"
+#                             "import libtbx.phil\n"
+#                             "out_dir=\"{}\"\n".format(out_dir) +
+#                             "xtal_name=\"{}\"\n".format(xtal_name) +
+#                             "with open(os.path.join(out_dir, " \
+#                             "xtal_name,\'{}_param.pickle\'.format(xtal_name)),'rb') as handle:\n"
+#                             "\tparams = pickle.load(handle)\n"
+#                             "exhaustive(params)")
+#         python_script.close()
+#
+#         # write bash script
+#         bash_script=open(os.path.join(out_dir, xtal_name,
+#                                      "{}_exhaustive.sh".format(xtal_name)),'w')
+#         bash_script.write("source /dls/science/groups/i04-1/software/" \
+#                           "pandda-update/ccp4/ccp4-7.0/setup-scripts/ccp4.setup-sh\n"
+#                           # "export PYTHONPATH=\"${PYTHONPATH}:" \
+#                           # "/dls/science/groups/i04-1/elliot-dev/"
+#                           # "Work/exhaustive_search/exhaustive\"\n"
+#                           "ccp4-python " + os.path.join(out_dir, xtal_name,
+#                                           "{}_exhaustive.py".format(xtal_name)))
+#         bash_script.close()
+#         # submit job
+#         os.system("qsub {}".format(os.path.join(out_dir, xtal_name,xtal_name+"_exhaustive.sh")))
 
 
 # Get exhaustive search minima fofc
