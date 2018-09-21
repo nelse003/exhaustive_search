2beff0e14067fd7fde05c4035c66fae6fa04eb3e
Elliot Nelson <elliot.nelson@dtc.ox.ac.uk>
2018-09-21 17:43:02 +0100

    edstats update to pull together dcp2b results

--- edstats_plots.py
+++ edstats_plots.py
@@ -3,7 +3,7 @@ import pandas as pd
 es_minima_csv = "/dls/science/groups/i04-1/elliot-dev/Work/exhaustive_search_data/DCP2B_18_09_20_exhaus/es_minima.csv"
 edstats_csv = "/dls/science/groups/i04-1/elliot-dev/Work/exhaustive_search_data/DCP2B_18_09_20_exhaus/edstats.csv"
 
-edstats_df = pd.read_csv(edstats_csv, columns =['Dataset','ES_occ','es_bfac''min_fofc'])
+edstats_df = pd.read_csv(edstats_csv, names=['Dataset','ES_occ','es_bfac''min_fofc'])
 es_minima_df = pd.read_csv(es_minima_csv)
 
 print(edstats_df)
