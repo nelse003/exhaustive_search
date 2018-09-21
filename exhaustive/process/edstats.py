4c3adfffff25b516250c1c2428589d046886c854
Elliot Nelson <elliot.nelson@dtc.ox.ac.uk>
2018-09-21 14:52:48 +0100

    edstats for dcp2b loop

--- exhaustive/process/edstats.py
+++ exhaustive/process/edstats.py
@@ -17,7 +17,7 @@ from exhaustive.exhaustive.utils.utils import get_xtals_from_db
 # such as GDP for janine
 
 #params.input.in_path = "/dls/labxchem/data/2016/lb13385-64/processing/analysis/initial_model"
-params.input.database_path = "/dls/labxchem/data/2016/lb13385-64/processing/database/soakDBDataFile.sqlite""
+params.input.database_path = "/dls/labxchem/data/2016/lb13385-64/processing/database/soakDBDataFile.sqlite"
 params.output.out_dir = "/dls/science/groups/i04-1/elliot-dev/Work/exhaustive_search_data/DCP2B_18_09_20_exhaus"
 
 for xtal_name, pdb, mtz in get_xtals_from_db(params,
