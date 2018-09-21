b7797ca51fe6894c73ffc32260de1af2f30f6e2f
Elliot Nelson <elliot.nelson@dtc.ox.ac.uk>
2018-09-21 18:03:04 +0100

    edstats update to pull together dcp2b results

--- edstats_plots.py
+++ edstats_plots.py
@@ -1,10 +1,13 @@
 import pandas as pd
+import seaborn as sns
 
 es_minima_csv = "/dls/science/groups/i04-1/elliot-dev/Work/exhaustive_search_data/DCP2B_18_09_20_exhaus/es_minima.csv"
 edstats_csv = "/dls/science/groups/i04-1/elliot-dev/Work/exhaustive_search_data/DCP2B_18_09_20_exhaus/edstats.csv"
 
 edstats_df = pd.read_csv(edstats_csv)
 es_minima_df = pd.read_csv(es_minima_csv, names=['Dataset','ES_occ','es_bfac','min_fofc'])
+
+summary_df = pd.merge(edstats_df,es_minima_df, on='Dataset')
 
 print(edstats_df)
 print(es_minima_df)
@@ -13,4 +16,8 @@ print(edstats_df['Dataset'])
 print(es_minima_df['Dataset'])
 
 print(pd.merge(edstats_df,es_minima_df, on='Dataset'))
+print(list(pd.merge(edstats_df,es_minima_df, on='Dataset')))
 
+pairplot = sns.pairplot(summary_df)
+sns_plot.savefig("/dls/science/groups/i04-1/elliot-dev/Work/exhaustive_search_data/DCP2B_18_09_20_exhaus/pairplot.png",
+                 dpi=600)
