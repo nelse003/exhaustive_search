496f24af02b57276693566a849954bedeb1e9725
Elliot Nelson <elliot.nelson@dtc.ox.ac.uk>
2018-09-21 17:08:05 +0100

    edstats.py

--- edstats.py
+++ edstats.py
@@ -21,39 +21,39 @@ from exhaustive.exhaustive.utils.utils import get_xtals_from_db
 # such as GDP for janine
 
 #params.input.in_path = "/dls/labxchem/data/2016/lb13385-64/processing/analysis/initial_model"
-params = master_phil.extract()
-
+# params = master_phil.extract()
+#
 params.input.database_path = "/dls/labxchem/data/2016/lb13385-64/processing/database/soakDBDataFile.sqlite"
 params.output.out_dir = "/dls/science/groups/i04-1/elliot-dev/Work/exhaustive_search_data/DCP2B_18_09_20_exhaus"
+#
+# for xtal_name, pdb, mtz in get_xtals_from_db(params,
+#                                              refinement_outcomes="'4 - CompChem ready', "
+#                                                                  "'5 - Deposition ready',"
+#                                                                  "'6 - Deposited'" ):
+#
+#     os.system("cp {} {}".format(pdb, os.path.join(params.output.out_dir,xtal_name,os.path.basename(pdb))))
+#     os.system("cp {} {}".format(mtz, os.path.join(params.output.out_dir, xtal_name, os.path.basename(mtz))))
+#     os.chdir(os.path.join(params.output.out_dir,xtal_name))
+#
+#     if not os.path.exists(os.path.join(params.output.out_dir,xtal_name,"residue_scores.csv")):
+#         os.system("giant.score_model {} {}".format(pdb, mtz))
+
+# Currently pandas is failing to import in ccp4 python so this is done seperately in a conda env
+
+dfs = []
 
 for xtal_name, pdb, mtz in get_xtals_from_db(params,
                                              refinement_outcomes="'4 - CompChem ready', "
                                                                  "'5 - Deposition ready',"
                                                                  "'6 - Deposited'" ):
 
-    os.system("cp {} {}".format(pdb, os.path.join(params.output.out_dir,xtal_name,os.path.basename(pdb))))
-    os.system("cp {} {}".format(mtz, os.path.join(params.output.out_dir, xtal_name, os.path.basename(mtz))))
-    os.chdir(os.path.join(params.output.out_dir,xtal_name))
+    edstats_csv = os.path.join(params.output.out_dir, xtal_name "residue_scores.csv")
+    edstats_df = pd.read_csv(edstats_csv)
+    edstats_df['Dataset'] = xtal_name
+    dfs.append(edstats_df)
 
-    if not os.path.exists(os.path.join(params.output.out_dir,xtal_name,"residue_scores.csv")):
-        os.system("giant.score_model {} {}".format(pdb, mtz))
 
-# Currently pandas is failing to import in ccp4 python so this is done seperately in a conda env
-
-# dfs = []
-#
-# for folder in os.listdir(ini_folder):
-#     if os.path.exists(os.path.join(ini_folder,folder,"refine.pdb")):
-#         edstats_csv = os.path.join(ini_folder, folder, "residue_scores.csv")
-#         edstats_df = pd.read_csv(edstats_csv)
-#         edstats_df['Dataset'] = folder
-#         dfs.append(edstats_df)
-#         print(folder)
-#         print(edstats_df)
-#         print(dfs)
-#
-#
-# print("-------------------------------------------")
-# compound_edstats = pd.concat(dfs, ignore_index=True)
-# print(compound_edstats)
-# compound_edstats.to_csv(os.path.join(ini_folder,'edstats.csv'))
+print("-------------------------------------------")
+compound_edstats = pd.concat(dfs, ignore_index=True)
+print(compound_edstats)
+compound_edstats.to_csv(os.path.join(params.output.out_dir,'edstats.csv'))
